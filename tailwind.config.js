c8ed9949ad5d6c89bd160d94b9dc725874445dc2
Muhammad Wildan Pratama <91406760+mwldnp@users.noreply.github.com>
2022-12-14 17:17:09 +0700

    Add project files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Space-Mono' : ['Space Mono'],
+        'Montserrat' : ['Montserrat'],
+      }
+    },
+  },
+  plugins: [],
+}
